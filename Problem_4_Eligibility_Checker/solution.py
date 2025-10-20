41fb25d7f221547dd1be4517fef8ab88ee73b446
dbu392 <ayenewasema@gmail.com>
2025-10-20 11:40:23 -0700

    Update solution.py

--- Problem_4_Eligibility_Checker/solution.py
+++ Problem_4_Eligibility_Checker/solution.py
@@ -1,1 +1,10 @@
-# your code here
+age = int(input("Enter your age: "))
+license_status = input("Do you have a driver's license? (yes/no): ").lower()
+
+if age >= 18:
+    if license_status == "yes":
+        print("You are eligible to drive.")
+    else:
+        print("You are old enough, but you need a driver's license to drive.")
+else:
+    print("You are not old enough to drive.")
