ad9cb6b9fa65e047352c3a65ac35bfd7d1ca4fbb
dbu392 <ayenewasema@gmail.com>
2025-10-20 10:53:03 -0700

    Update solution.py

--- Problem_2_Unit_Converter/solution.py
+++ Problem_2_Unit_Converter/solution.py
@@ -1,1 +1,14 @@
-# your code here
+print("Which conversion would you like to do?")
+print("0. Meters to Centimeters")
+print("1. Centimeters to Meters")
+choice = input("Enter 0 or 1: ")
+if choice == "0":
+    meters = float(input("Enter value in meters: "))
+    centimeters = meters * 100
+    print("Value in centimeters: " + str(centimeters) + "cm")
+elif choice == "1":
+    centimeters = float(input("Enter value in centimeters: "))
+    meters = centimeters / 100
+    print("Value in meters: " + str(meters) + "m")
+else:
+    print("Invalid choice.")
