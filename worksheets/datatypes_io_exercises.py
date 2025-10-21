b673d974848fb94809cf7c97a8e5f7fea117cb72
dbu392 <ayenewasema@gmail.com>
2025-10-21 10:11:31 +0300

    Update datatypes_io_exercises.py

--- worksheets/datatypes_io_exercises.py
+++ worksheets/datatypes_io_exercises.py
@@ -1,2 +1,15 @@
-print("Datatype & I/O Exercises")
-# Practice input/output and type conversions here.
+a=input("enter the name: ")
+b=input("enter the number: ")
+c=input("enter the number: ")
+d=input("enter the value: ")
+e=input("enter the complex: ")
+a=("brother")
+b=4
+c=4.5
+d=True
+e=3+4j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
